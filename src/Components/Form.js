1f58056c6d287f98b0a0f1bb16caa29467712163
khushboo <khushboo20@navgurukul.org>
2023-10-30 16:09:50 +0530

    Add emailjs in data array

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,6 +2,8 @@ import { Button, TextField, Typography } from "@mui/material";
 import { useState} from "react";
 import { useRef } from "react";
 
+import emailjs from '@emailjs/browser';
+
 function Form() {
 
     const [userdata, setUserData] = useState([])
@@ -39,24 +41,25 @@ function Form() {
         setPhone("")
         useUserRef.current.focus()
         setUserData([...userdata,{name,phone,email,massege}])
+        emailjs.send("service_bf0svzw","template_m6bcxbm", data,"4d5i6ZOR1ZcvEGMxD")
         
 
 
     }
-
+    
     console.log(userdata,"data")
      
     return (
         <>
         <Typography>Gmaill Information</Typography>
-            <TextField  ref={useUserRef} label="Name...." onChange={HandlerName} value={name}></TextField><br></br>
+            <TextField name="name" ref={useUserRef} label="Name...." onChange={HandlerName} value={name}></TextField><br></br>
 
-            <TextField  ref={useUserRef} label="Emaill...." onChange={HanldeEmail} value={email}></TextField><br></br>
+            <TextField name="emaill" ref={useUserRef} label="Emaill...." onChange={HanldeEmail} value={email}></TextField><br></br>
 
             <TextField ref={useUserRef} label="phone...." onChange={HanlderPhone} value={phone}></TextField><br></br>
 
             <TextField
-
+          
                 ref={useUserRef}
                 id="outlined-multiline-static"
                 label="Multiline"
