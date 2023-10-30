bc7aacd2cf4c2c156731ea2b1fd0a34ad67ed574
khushboo <khushboo20@navgurukul.org>
2023-10-30 12:15:06 +0530

    Store user information in Array

---
+++ src/Components/Form.js
@@ -0,0 +1,75 @@
+import { Button, TextField, Typography } from "@mui/material";
+import { useState} from "react";
+import { useRef } from "react";
+
+function Form() {
+
+    const [userdata, setUserData] = useState([])
+    const [name, setName] = useState("")
+
+    const [email, setEmail] = useState("")
+    const [massege, setMassege] = useState("")
+
+    const [phone, setPhone] = useState("")
+
+    const useUserRef=useRef('')
+
+    const HandlerName = (e) => {
+        setName(e.target.value)
+    }
+
+    const HanlderPhone = (e) => {
+        setPhone(e.target.value)
+    }
+
+    const HanldeEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const HandlerMassge=(e)=>{
+        setMassege(e.target.value)
+    }
+
+    const HandlerSave=(e)=>{
+
+        e.preventDefault()
+        setEmail('')
+        setMassege('')
+        setName("")
+        setPhone("")
+        useUserRef.current.focus()
+        setUserData([...userdata,{name,phone,email,massege}])
+        
+
+
+    }
+
+    console.log(userdata,"data")
+     
+    return (
+        <>
+        <Typography>Gmaill Information</Typography>
+            <TextField  ref={useUserRef} label="Name...." onChange={HandlerName} value={name}></TextField><br></br>
+
+            <TextField  ref={useUserRef} label="Emaill...." onChange={HanldeEmail} value={email}></TextField><br></br>
+
+            <TextField ref={useUserRef} label="phone...." onChange={HanlderPhone} value={phone}></TextField><br></br>
+
+            <TextField
+
+                ref={useUserRef}
+                id="outlined-multiline-static"
+                label="Multiline"
+                multiline
+                rows={4}
+                onChange={HandlerMassge}
+                value={massege}
+
+            /><br></br>
+
+            <Button onClick={HandlerSave} variant="contained">save</Button><br></br>
+
+        </>
+    )
+}
+export default Form;
